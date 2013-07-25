98fe2bb0a49c0a8fd819b8e0d7e119be47172047
Alexander Stehlik <alexander.stehlik@googlemail.com>
2013-07-25 12:47:21 +0200

    [FEATURE] Content element wizard

--- ext_localconf.php
+++ ext_localconf.php
@@ -26,6 +26,25 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['Sto\\Mediaoembe
 	'description'      => 'Creates non-existing providers with the name, the url scheme and the endpoint from embed.ly.',
 );
 
+
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['tx_mediaoembed_migratecontentelements'] = 'Sto\\Mediaoembed\\Install\\MigrateContentElementsUpdate';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+mod.wizards.newContentElement {
+	wizardItems {
+		common.elements {
+			mediaoembed_oembedmediarenderer {
+				icon = gfx/c_wiz/multimedia.gif
+				title = LLL:EXT:mediaoembed/Resources/Private/Language/locallang_db.xlf:tt_content.CType.I.tx_mediaoembed
+				description = LLL:EXT:mediaoembed/Resources/Private/Language/locallang_db.xlf:new_content_element_wizard_oembedmediarenderer_description
+				tt_content_defValues {
+					CType = mediaoembed_oembedmediarenderer
+				}
+			}
+		}
+		common.show := addToList(mediaoembed_oembedmediarenderer)
+	}
+}
+');
 
 ?>
