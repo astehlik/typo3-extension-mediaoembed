fc79ae765beae21b388a1fc7fac939626680ed7a
Alexander Stehlik <alexander.stehlik@googlemail.com>
2013-09-20 15:18:27 +0200

    [TASK] Removed remaining import tasks
    The tasks are migrated to a command controller which can also be called
    in the scheduler.

--- ext_localconf.php
+++ ext_localconf.php
@@ -14,18 +14,6 @@ $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['getData'][] =
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
 );
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['Sto\\Mediaoembed\\Tasks\\ImportFromEmbedlyTask'] = array(
-	'extension'        => $_EXTKEY,
-	'title'            => 'Import providers from embed.ly',
-	'description'      => 'Creates / updates the name, description and url schemes of the oEmbed providers from embed.ly.',
-);
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['Sto\\Mediaoembed\\Tasks\\ImportFromOhhembedTask'] = array(
-	'extension'        => $_EXTKEY,
-	'title'            => 'Import providers from oohembed.com',
-	'description'      => 'Creates non-existing providers with the name, the url scheme and the endpoint from embed.ly.',
-);
-
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['tx_mediaoembed_createrequiredcolumns'] = 'Sto\\Mediaoembed\\Install\\CreateRequiredColumnsUpdate';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['tx_mediaoembed_migratecontentelements'] = 'Sto\\Mediaoembed\\Install\\MigrateContentElementsUpdate';
 
