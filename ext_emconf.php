d6b33657ce23ecaa7425dca2f1ef20fe2209461d
Alexander Stehlik <alexander.stehlik@gmail.com>
2018-06-23 14:49:50 +0200

    [!!!][TASK] Require PHP 7+ and TYPO3 7.6+

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '7.6.0-9.4.99',
             'extbase' => '',
         ],
         'conflicts' => [],
