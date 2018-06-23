db5bd023c21d68a0a01c06a378d7d978b3749de0
Alexander Stehlik <alexander.stehlik@gmail.com>
2018-06-23 14:48:23 +0200

    [TASK] Streamline Travis build
    Use build stages and parallel builds.
    
    Remove obsolete dependencies from composer.json.

---
+++ .travis/test_phpunit.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -ev
+
+echo "No Unit tests yet :("
+exit 0
+
+echo "Running unit tests";
+
+if [ ${TYPO3_VERSION} = "typo3/minimal=dev-master" ]; then
+    composer config minimum-stability dev && composer require ${TYPO3_VERSION} --prefer-stable --prefer-dist
+else
+    composer require ${TYPO3_VERSION}
+fi
+
+
+if [[ "${UPLOAD_CODE_COVERAGE}" == "yes" ]]; then
+    .Build/bin/phpunit --coverage-clover .Build/Logs/clover.xml --whitelist Classes Tests/Unit/
+else
+    phpenv config-rm xdebug.ini
+    .Build/bin/phpunit Tests/Unit/
+fi
+
+if [[ "${UPLOAD_CODE_COVERAGE}" == "yes" ]]; then
+    composer require --dev codeclimate/php-test-reporter:dev-master;
+    .Build/bin/test-reporter --coverage-report .Build/Logs/clover.xml;
+fi
