a28a62be22c58d1371ff4ac1cf39b55d1fefc2e8
Alexander Stehlik <alexander.stehlik@gmail.com>
2018-06-24 11:44:55 +0200

    [TASK] Register default provider TypoScript

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -6,3 +6,9 @@ defined('TYPO3_MODE') or die();
     'Configuration/TypoScript',
     'Media oEmbed'
 );
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'mediaoembed',
+    'Configuration/TypoScript/DefaultProviders',
+    'Media oEmbed default providers'
+);
