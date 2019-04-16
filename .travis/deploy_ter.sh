85b3c5b17423cbee389d0d367fad2c9482becf8e
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-04-16 09:27:30 +0200

    [TASK] Update / fix TER deployment script

--- .travis/deploy_ter.sh
+++ .travis/deploy_ter.sh
@@ -1,40 +1,52 @@
 #!/usr/bin/env bash
 
+extensionName="mediaoembed"
+projectDirectory="typo3-extension-mediaoembed"
+
 set -ev
 
 phpenv config-rm xdebug.ini
 
-# Rename our working directory, required for Extension upload to TER.
-cd .. && mv typo3-extension-mediaoembed mediaoembed
+echo "Cleanup Git repository..."
+git reset --hard HEAD && git clean -fx
 
-if [ -z "$TRAVIS_TAG" ]; then
+if [[ -z "$TRAVIS_TAG" ]]; then
     echo "No Travis tag is available. Upload only runs for new tags."
     exit 0
 fi
 
-if [ -z "$TYPO3_ORG_USERNAME" ]; then
+if [[ -z "$TYPO3_ORG_USERNAME" ]]; then
     echo "The $TYPO3_ORG_USERNAME env var is not set."
     exit 1
 fi
 
-if [ -z "$TYPO3_ORG_PASSWORD" ]; then
+if [[ -z "$TYPO3_ORG_PASSWORD" ]]; then
     echo "The $TYPO3_ORG_PASSWORD env var is not set."
     exit 1
 fi
 
-TAG_MESSAGE=`git tag -n10 -l $TRAVIS_TAG | sed 's/^[0-9.]*[ ]*//g'`
+TAG_MESSAGE=`git tag -n10 -l ${TRAVIS_TAG} | sed 's/^v[0-9.]*[ ]*//g'`
 
-if [ -z "$TAG_MESSAGE" ]; then
+if [[ -z "$TAG_MESSAGE" ]]; then
     echo "The tag message could not be detected or was empty."
     exit 1
 fi
+
+echo "Extracted tag message: $TAG_MESSAGE"
+
+
+echo "Renaming repository folder to match extension key..."
+cd ..
+mv "${projectDirectory}" "${extensionName}"
 
 echo "Installing TYPO3 repository client..."
 
 composer create-project --no-dev namelesscoder/typo3-repository-client typo3-repository-client
 
+cd ${extensionName}
+
+echo "Setting version to ${TRAVIS_TAG#"v"}"
+../typo3-repository-client/bin/setversion ${TRAVIS_TAG#"v"}
+
 echo "Uploading release ${TRAVIS_TAG} to TER"
-
-./typo3-repository-client/bin/setversion ${TRAVIS_TAG} stable mediaoembed
-
-./typo3-repository-client/bin/upload mediaoembed "$TYPO3_ORG_USERNAME" "$TYPO3_ORG_PASSWORD" "$TAG_MESSAGE"
+../typo3-repository-client/bin/upload . "$TYPO3_ORG_USERNAME" "$TYPO3_ORG_PASSWORD" "$TAG_MESSAGE"
