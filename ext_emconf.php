6050d794a12c06c6f5e9a1821064c2f4b480976d
Alexander Stehlik <alexander.stehlik@gmail.com>
2019-05-15 00:17:04 +0200

    [BUGFIX] Raise maximum allowed TYPO3 version in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-9.4.99',
+            'typo3' => '7.6.0-9.5.99',
             'extbase' => '',
         ],
         'conflicts' => [],
