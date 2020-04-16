944ac084015a42564ffc4d3d37ef1bc2a6e9020e
Alexander Stehlik <alexander.stehlik@gmail.com>
2020-04-16 20:28:31 +0200

    [TASK] Use Travis MySQL service

--- .travis/test_functional.sh
+++ .travis/test_functional.sh
@@ -16,8 +16,10 @@ mkdir -p ".Build/Web/typo3temp/var/tests"
 
 export typo3DatabaseName="typo3"
 export typo3DatabaseHost="localhost"
-export typo3DatabaseUsername="root"
+export typo3DatabaseUsername="travis"
 export typo3DatabasePassword=""
+
+mysql -e 'CREATE DATABASE typo3;'
 
 find . -wholename '*Tests/Functional/*Test.php' ! -path "./.Build/*" ! -path "./Tests/Functional/AbstractFunctionalTest.php" | \
     parallel --gnu 'echo; echo "Running functional test suite {}"; .Build/bin/phpunit --colors -c .Build/vendor/nimut/testing-framework/res/Configuration/FunctionalTests.xml {}'
