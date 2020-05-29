68e3b4f460e31f4ca3514645d6e854bbd1cf641e
Alexander Stehlik <alexander.stehlik@gmail.com>
2020-05-29 16:53:58 +0200

    [CLEANUP] Code cleanup in ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,7 +2,7 @@
 # Table structure for table 'tt_content'
 #
 CREATE TABLE tt_content (
-	tx_mediaoembed_maxwidth int(11) unsigned DEFAULT '0' NOT NULL,
-	tx_mediaoembed_maxheight int(11) unsigned DEFAULT '0' NOT NULL,
-	tx_mediaoembed_url varchar(255) DEFAULT '' NOT NULL
+	tx_mediaoembed_maxwidth INT(11) UNSIGNED DEFAULT '0' NOT NULL,
+	tx_mediaoembed_maxheight INT(11) UNSIGNED DEFAULT '0' NOT NULL,
+	tx_mediaoembed_url VARCHAR(255) DEFAULT '' NOT NULL
 );
