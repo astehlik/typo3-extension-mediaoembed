457d372ce07a31762fe42624930d4f9040858b89
Alexander Stehlik <alexander.stehlik@gmail.com>
2021-05-26 16:58:21 +0200

    [TASK] Improve flexibility in embedResponsivePadding view helper
    Use tag based view helper, allow custom classes
    allow custom style tag.

--- Classes/ViewHelpers/EmbedResponsivePaddingViewHelper.php
+++ Classes/ViewHelpers/EmbedResponsivePaddingViewHelper.php
@@ -4,8 +4,9 @@ namespace Sto\Mediaoembed\ViewHelpers;
 
 use Sto\Mediaoembed\Content\Configuration;
 use Sto\Mediaoembed\Response\Contract\AspectRatioAwareResponseInterface;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
 
-class EmbedResponsivePaddingViewHelper extends AbstractViewHelper
+class EmbedResponsivePaddingViewHelper extends AbstractTagBasedViewHelper
 {
     protected $escapeOutput = false;
 
@@ -16,19 +17,31 @@ class EmbedResponsivePaddingViewHelper extends AbstractViewHelper
 
     public function __construct(Configuration $configuration)
     {
+        parent::__construct();
         $this->configuration = $configuration;
     }
 
     public function initializeArguments()
     {
         $this->registerArgument('response', AspectRatioAwareResponseInterface::class, '', true);
+        $this->registerArgument('style-property', 'string', '', false, 'padding-top');
+
+        $this->registerTagAttribute(
+            'class',
+            'string',
+            'CSS class(es) for this element',
+            false,
+            'tx-mediaoembed-embed-responsive-padding'
+        );
     }
 
     public function render(): string
     {
         $aspectRatio = $this->getAspectRatio();
         $paddingTop = 100 / $aspectRatio . '%';
-        return '<div class="tx-mediaoembed-embed-responsive-padding" style="padding-top: ' . $paddingTop . '"></div>';
+        $this->tag->addAttribute('style', $this->arguments['style-property'] . ': ' . $paddingTop . ';');
+        $this->tag->setContent($this->renderChildren());
+        return $this->tag->render();
     }
 
     private function getAspectRatio(): float
