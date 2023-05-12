adabb4ca8dd5db59ca17620c487b513ace491ab1
Alexander Stehlik <alex@stehlik-online.de>
2023-05-12 11:49:18 +0200

    [TASK] Adjust dependencies for TYPO3 11

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,8 +10,8 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-10.4.99',
-            'extbase' => '',
+            'php' => '7.4.0-8.2.99',
+            'typo3' => '11.5.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
