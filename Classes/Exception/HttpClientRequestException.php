aaf412a2ff1b662b6cdb242ba5f87aea5b46cdc1
Alexander Stehlik <alex@stehlik-online.de>
2024-08-10 13:16:01 +0200

    [!!!][TASK] Remove error details from HttpClientRequestException

--- Classes/Exception/HttpClientRequestException.php
+++ Classes/Exception/HttpClientRequestException.php
@@ -9,19 +9,8 @@ use Throwable;
 
 class HttpClientRequestException extends RuntimeException
 {
-    /**
-     * @deprecated The param $errorDetails is ignored and will be removed in version 12.0.0.
-     */
-    public function __construct(string $message, int $httpCode, ?Throwable $previous = null, string $errorDetails = '')
+    public function __construct(string $message, int $httpCode, ?Throwable $previous = null)
     {
         parent::__construct($message, $httpCode, $previous);
-    }
-
-    /**
-     * @deprecated Not used any more, will always return an empty string. Will be removed in version 12.0.0.
-     */
-    public function getErrorDetails(): string
-    {
-        return '';
     }
 }
