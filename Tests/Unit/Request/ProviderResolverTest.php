7a31e142d6e1e54bd330829a1c7b3f5927193e74
Alexander Stehlik <alex@stehlik-online.de>
2024-11-09 12:56:22 +0100

    [TASK] Enhance tests for provider resolver

--- Tests/Unit/Request/ProviderResolverTest.php
+++ Tests/Unit/Request/ProviderResolverTest.php
@@ -12,26 +12,44 @@ use Sto\Mediaoembed\Tests\Unit\AbstractUnitTestCase;
 class ProviderResolverTest extends AbstractUnitTestCase
 {
     /**
-     * @var array|Provider[]
+     * @var array<Provider>
      */
-    private $dummyProviders;
+    private array $dummyProviders;
 
-    /**
-     * @var ProviderResolver
-     */
-    private $providerResolver;
+    private ProviderResolver $providerResolver;
 
     protected function setUp(): void
     {
-        $firstProvider = new Provider('test1', 'https://endpoint', ['testurl'], false);
-        $secondProvider = new Provider('test2', 'https://endpoint2', ['testurl'], false);
-
         $this->dummyProviders = [
-            $firstProvider,
-            $secondProvider,
+            new Provider('test1', 'https://endpoint', ['testurl'], false),
+            new Provider('test2', 'https://endpoint2', ['testurl'], false),
+            new Provider('wildcard', 'https://endpoint_wildcard', ['wildcard*test'], false),
+            new Provider('http', 'https://endpoint_http', ['http://url_with_http'], false),
+            new Provider('regex', 'https://endpoint_regex', ['#https?://my\\.provider\\.tld/playlist.*#i'], true),
         ];
 
         $this->providerResolver = new ProviderResolver($this->dummyProviders);
+    }
+
+    public function testResolverMatchesHttpsUrlIfHttpUrlIsConfigured(): void
+    {
+        $provider = $this->providerResolver->getNextMatchingProvider('https://url_with_http');
+
+        self::assertSame($this->dummyProviders[3], $provider);
+    }
+
+    public function testResolverMatchesRegexUrl(): void
+    {
+        $provider = $this->providerResolver->getNextMatchingProvider('https://my.provider.tld/playlist/blabla');
+
+        self::assertSame($this->dummyProviders[4], $provider);
+    }
+
+    public function testResolverMatchesUrlWithWildcard(): void
+    {
+        $provider = $this->providerResolver->getNextMatchingProvider('wildcard_fill_test');
+
+        self::assertSame($this->dummyProviders[2], $provider);
     }
 
     public function testResolverReturnsSecondMatchingProviderOnSecondCall(): void
