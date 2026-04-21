70b4c1d376bcba0723edb8893c5b824f9099bb2a
Alexander Stehlik <alex@stehlik.it>
2026-04-21 19:56:08 +0200

    chore: move icon registration to config file

---
+++ Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'extensions-mediaoembed-content-externalmedia' => [
+        'provider' => SvgIconProvider::class,
+        ['source' => 'EXT:mediaoembed/Resources/Public/Icons/ContentMedia.svg']
+    ],
+];
