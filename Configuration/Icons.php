e8ceefb9c74a6e0cf87a81f9f2d55c8419a1bb35
Alexander Stehlik <alex@stehlik.it>
2026-04-22 02:45:19 +0200

    fix: repair icon configuration

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -7,6 +7,6 @@ use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 return [
     'extensions-mediaoembed-content-externalmedia' => [
         'provider' => SvgIconProvider::class,
-        ['source' => 'EXT:mediaoembed/Resources/Public/Icons/ContentMedia.svg'],
+        'source' => 'EXT:mediaoembed/Resources/Public/Icons/ContentMedia.svg',
     ],
 ];
